7fc11ccdd0a2a1b602c99c23c054bed7b561ded6
mykel4ai-ship-it <mykel4ai@gmail.com>
2026-05-06 15:59:38 +0400

    Add default environment variable values
    Set default values for API_ID, API_HASH, BOT_TOKEN, and BOT_USERNAME in ENV_VARS class.

--- bot/__init__.py
+++ bot/__init__.py
@@ -10,10 +10,10 @@ logging.getLogger("urllib3").setLevel(logging.WARNING)
 
 
 class ENV_VARS(object):
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME")
+    API_ID = int(os.environ.get("API_ID", "38419498"))
+    API_HASH = os.environ.get("API_HASH", "e3e01a2954e405289817fac603af6196")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8778676653:AAG9u3T6RDTzxb-4pir-Ma8B22u5FIz5z7M")
+    BOT_USERNAME = os.environ.get("litkuBot")
     #AUTH_USER = int(os.environ.get("AUTH_USER", 5071059420))
 
 
